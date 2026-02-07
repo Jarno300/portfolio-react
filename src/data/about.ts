e07a0f7e20c492f6dd93e22b8dbed5c719689ce0
Jarno300 <r0467333@ucll.be>
2026-02-07 14:20:39 +0100

    about data

---
+++ src/data/about.ts
@@ -0,0 +1,12 @@
+interface About {
+  name: string;
+  title: string;
+  description: string;
+}
+
+export const about: About = {
+  name: "Jarno Mommens",
+  title: "Software Developer",
+  description:
+    "Passionate software developer with a background in customer relations. Experienced in B2B relationships, data analysis, and working as a team. Very eager to leverage these skills as a software developer.",
+};
